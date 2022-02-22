241180a5ce9b849d6aebd351e36621d43e7d6d48
Dasuni Jayathilake <dasunijayathilake@gmail.com>
2022-02-23 01:05:52 +0530

    visitor homepage updated

---
+++ client/src/components/visitor/visitorFooter/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../../../style/visitorHome/Home.css';
+
+export default function Footer() {
+  return (
+    <div>
+
+        <section id="footer">
+
+            <div class="share">
+                <a href="#" class="media"><i class="fab fa-facebook-f"></i></a>
+                <a href="#" class="media"><i class="fab fa-twitter"></i></a>
+                <a href="#" class="media"><i class="fab fa-instagram"></i></a>
+                <a href="#" class="media"><i class="fab fa-pinterest-p"></i></a>
+                <a href="#" class="media"><i class="fab fa-linkedin-in"></i></a>
+            </div>
+            <h1 class="credit"> Created by <span> Dasuni Jayathilake </span> | All rights reserved! </h1>
+        </section>
+
+    </div>
+  )
+}
--- client/src/components/visitor/visitorHome/Home.js
+++ client/src/components/visitor/visitorHome/Home.js
@@ -7,6 +7,7 @@ import step3 from '../../../assets/visitorHome/qr.png';
 import step4 from '../../../assets/visitorHome/meet.png';
 import Navigation from '../VisitorNavigation/Navigation';
 // import '../../../style/visitorHome/home.css';
+import Footer from '../visitorFooter/Footer';
 
 export default function Home() {
 
@@ -16,45 +17,53 @@ export default function Home() {
 
         <section class="home" id="home">
 
-<div className="content">
-    <h3>Meet Your <span id='host'>Service Provider...</span></h3>
-    <p>We bring it super faster to meet your service providers in a most secure way. You don't have to worry about the waste of time anymore. Simply save a time slot for you! </p>
-    <a href="#" className="btn" id='started'>order now</a>
-</div>
+            <div className="content">
+                <h3>Meet Your <span id='host'>Service Provider...</span></h3>
+                <p>We bring it super faster to meet your service providers in a most secure way. You don't have to worry about the waste of time anymore. Simply save a time slot for you! </p>
+                <a href="#" className="btn" id='started'><span>Appoint Here</span></a>
+            </div>
 
-<div className="image">
-    <img src={home} alt="home-image"/>
-</div>
+            <div className="image">
+                <img src={home} alt="home-image"/>
+            </div>
 
-</section>
+        </section>
 
 
-<div className="step-container">
+        <div className="step-container">
 
-<h1 className="heading">how it <span>works</span></h1>
+            <h1 className="heading">how it <span>works</span></h1>
 
-<section className="steps">
+            <section className="steps">
 
-    <div className="box">
-        <img src={step1} />
-        <h3>Check host schedule</h3>
-    </div>
-    <div className="box">
-        <img src={step2} />
-        <h3>Make appointments</h3>
-    </div>
-    <div className="box">
-        <img src={step3} />
-        <h3>QR code login</h3>
-    </div>
-    <div className="box">
-        <img src={step4} />
-        <h3>Meet your host</h3>
-    </div>
+                <div className="box">
+                    <img src={step1} />
+                    <h3>Check host schedule</h3>
+                </div>
 
-</section>
+                <div className="box">
+                    <img src={step2} />
+                    <h3>Make appointments</h3>
+                </div>
 
-</div>
+                <div className="box">
+                    <img src={step3} />
+                    <h3>QR code login</h3>
+                </div>
+
+                <div className="box">
+                    <img src={step4} />
+                    <h3>Meet your host</h3>
+                </div>
+
+            </section>
+
+        </div>
+
+        <Footer />
+
+        <a href="#home" class="fas fa-angle-up" id="scroll-top"></a>
+
     </div>
 
 
